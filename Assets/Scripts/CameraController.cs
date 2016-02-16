24d0514fa3a07dbebde8c00949ee0083077a9758
poudigne <pierluc0414@live.ca>
2016-02-16 14:45:35 -0500

    Player Movement and camera follow

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public PlayerControl player;
+
+    private Vector3 lastPlayerPos;
+    private float distanceToMove;
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerControl>();
+        lastPlayerPos = player.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        distanceToMove = player.transform.position.x - lastPlayerPos.x;
+        transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+
+        lastPlayerPos = player.transform.position;
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+    public bool grounded;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D body;
+    private Collider2D collider;
+    private Animator animator;
+
+	// Use this for initialization
+	void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        grounded = Physics2D.IsTouchingLayers(collider, groundLayer);
+        body.velocity = new Vector2(moveSpeed, body.velocity.y);
+
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) && grounded)
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpForce);
+        }
+        animator.SetFloat("Speed", body.velocity.x);
+        animator.SetBool("Grounded", grounded);
+
+    }
+}
